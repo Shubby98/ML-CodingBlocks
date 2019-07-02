59d34a9883e4c5ced94b933e7c035eaff5ba2994
shubham <shubby98@gmail.com>
2019-07-02 08:18:02 +0530

    lecture 11 added

---
+++ challenges/snapchat filter/filter.py
@@ -0,0 +1,10 @@
+import cv2 
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+
+kingslayer = cv2.imread("Train/Jamie_Before.jpg")
+cv2.imshow("jamie",kingslayer)
+
+cam.release()
+cv2.destroyAllWindows()
